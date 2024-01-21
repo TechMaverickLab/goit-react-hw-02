093a55454502fbc7f55ae157e9084f6b11ec13fe
TechMaverickLab <TechMaverickLab@gmail.com>
2024-01-21 09:07:31 -0500

    додавання кнопки Reset

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,15 @@ import Options from './components/Options.jsx';
 import Notification from './components/Notification.jsx';
 
 function App() {
-    const [feedback, setFeedback] = useState({ good: 0, neutral: 0, bad: 0 });
+    const initialFeedback = { good: 0, neutral: 0, bad: 0 };
+    const [feedback, setFeedback] = useState(initialFeedback);
 
     const handleFeedback = (type) => {
         setFeedback({ ...feedback, [type]: feedback[type] + 1 });
+    };
+
+    const resetFeedback = () => {
+        setFeedback(initialFeedback);
     };
 
     const totalFeedback = feedback.good + feedback.neutral + feedback.bad;
@@ -23,7 +28,7 @@ function App() {
         <div>
             <h1>Sip Happens Café</h1>
             <p>Please leave your feedback about our service by selecting one of the options below.</p>
-            <Options onFeedback={handleFeedback} options={feedbackOptions} />
+            <Options onFeedback={handleFeedback} onReset={resetFeedback} options={feedbackOptions} />
             {totalFeedback > 0 ? (
                 <Feedback feedback={feedback} total={totalFeedback} positivePercentage={positivePercentage} />
             ) : (
--- src/components/Options.jsx
+++ src/components/Options.jsx
@@ -1,27 +1,24 @@
 // import React from 'react';
 import PropTypes from 'prop-types';
 
-function Options({ onFeedback, options }) {
+function Options({ onFeedback, onReset, options }) {
     return (
         <div>
-            {options.map(option => (
-                <button key={option.type} onClick={() => onFeedback(option.type)}>
-                    {option.name}
-                </button>
+            {options.map(({ name, type }) => (
+                <button key={type} onClick={() => onFeedback(type)}>{name}</button>
             ))}
+            <button onClick={onReset}>Reset</button>
         </div>
     );
 }
 
 Options.propTypes = {
     onFeedback: PropTypes.func.isRequired,
-    options: PropTypes.arrayOf(
-        PropTypes.shape({
-            name: PropTypes.string.isRequired,
-            type: PropTypes.string.isRequired
-        })
-    ).isRequired
+    onReset: PropTypes.func.isRequired,
+    options: PropTypes.arrayOf(PropTypes.shape({
+        name: PropTypes.string.isRequired,
+        type: PropTypes.string.isRequired
+    })).isRequired
 };
-
 
 export default Options;
